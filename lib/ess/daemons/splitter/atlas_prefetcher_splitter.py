ba67c176e2ea831b3dfcc2d526bce7eecef7fbdb
Wen Guan <wen.guan@cern.ch>
2019-02-27 18:50:17 +0100

    add splitter

---
+++ lib/ess/daemons/splitter/atlas_prefetcher_splitter.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0OA
+#
+# Authors:
+# - Wen Guan, <wen.guan@cern.ch>, 2019
+
+
+"""
+Splitter plugin based ATLAS AthenaMP prefetcher
+"""
+
+import datetime
+import logging
+import threading
+import time
+import traceback
+import yampl
+import Queue
+
+from ess.common.utils import run_process
+from ess.daemons.common.plugin_base import PluginBase
+
+
+class AtlasPrefetcher(threading.Thread):
+
+    class MessageThread(threading.Thread):
+        def __init__(self, messageQ, socketname, context='local', logger=None, **kwds):
+            threading.Thread.__init__(self, **kwds)
+            if logger:
+                self.logger = logger
+            else:
+                self.logger = logging.getLogger(self.__class__.__name__)
+            self.messageQ = messageQ
+            self.num_messages_required = 0
+            self._stop = threading.Event()
+            try:
+                self.__messageSrv = yampl.ServerSocket(socketname, context)
+            except:
+                self.logger.debug("Exception: failed to start yampl server socket: %s" % traceback.format_exc())
+
+        def send(self, message):
+            try:
+                self.__messageSrv.send_raw(message)
+                self.num_messages_required -= 1
+            except:
+                self.logger.debug("Exception: failed to send yampl message: %s" % traceback.format_exc())
+
+        def stop(self):
+            self._stop.set()
+
+        def stopped(self):
+            return self._stop.isSet()
+
+        def __del__(self):
+            if self.__messageSrv:
+                del self.__messageSrv
+                self.__messageSrv = None
+
+        def run(self):
+            try:
+                while True:
+                    if self.stopped():
+                        if self.__messageSrv:
+                            del self.__messageSrv
+                            self.__messageSrv = None
+                        break
+                    size, buf = self.__messageSrv.try_recv_raw()
+                    if size == -1:
+                        time.sleep(0.00001)
+                    else:
+                        self.logger.info("Received message: %s" % buf)
+                        if 'Ready for events' in buf:
+                            self.num_messages_required += 1
+                        else:
+                            self.messageQ.put(buf)
+            except:
+                self.logger.debug("Exception: Message Thread failed: %s" % traceback.format_exc())
+                if self.__messageSrv:
+                    del self.__messageSrv
+                    self.__messageSrv = None
+
+    def __init__(self, request_queue, output_queue, process_name, process_cmd, stdout, stderr, num_threads):
+        threading.Thread.__init__(self)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.graceful_stop = threading.Event()
+
+        self.process_name = process_name
+        self.process_cmd = process_cmd
+        self.num_threads = num_threads
+        self.stdout = stdout
+        self.stderr = stderr
+        self.process = None
+        self.message_thread = None
+
+        self.request_queue = request_queue
+        self.output_queue = output_queue
+
+    def start_prefetcher_process(self):
+        """
+        Start AthenaMP prefetcher process.
+        """
+        self.logger.info("Starting process(stdout: %s, stderr: %s): %s" % (self.stdout, self.stderr, self.process_cmd))
+        self.process = run_process(self.process_cmd, stdout=self.stdout, stderr=self.stderr)
+
+        self.logger.info("Starting messaging thread.")
+        self.message_thread = AtlasPrefetcher.MessageThread(self.output_queue, socketname=self.process_name, logger=self.logger)
+        self.message_thread.start()
+
+    def stop(self):
+        self.graceful_stop.set()
+
+    def inject_request(self):
+        if not self.request_queue.empty():
+            req = self.request_queue.get(False)
+            if req:
+                req = str([req])
+                self.logger.info("Inject a message to prefetcher: %s" % req)
+                self.message_thread.send(req)
+
+    def run(self):
+        self.start_prefetcher_process()
+        time_logging = time.time()
+        while not self.graceful_stop.is_set():
+            ret_code = self.process.poll()
+            if ret_code is None:
+                if time.time() - time_logging > 60:
+                    self.logger.info("Prefetcher process is still running.")
+                    time_logging = time.time()
+            else:
+                self.logger.info("Prefecther process finished with %s" % ret_code)
+                break
+
+            while (self.message_thread.num_messages_required > 0 and not self.request_queue.empty()):
+                self.inject_request()
+            time.sleep(0.1)
+
+        self.logger.info("Stopping messaging thread.")
+        self.message_thread.stop()
+
+        self.logger.info("Stopping prefetcher process.")
+        self.process.terminate()
+        while self.process.poll() is None:
+            self.logger.info("Prefetcher is still running")
+            time.sleep(5)
+        self.logger.info("Prefetcher terminated")
+
+
+class AtlasPrefetcherSplitter(PluginBase, threading.Thread):
+    def __init__(self, **kwargs):
+        threading.Thread.__init__(self)
+        super(AtlasPrefetcherSplitter, self).__init__(**kwargs)
+        self.setup_logger()
+        self.graceful_stop = threading.Event()
+
+        if not hasattr(self, 'num_threads'):
+            self.num_threads = 1
+
+        if not hasattr(self, 'output_prefix'):
+            self.output_prefix = 'splitted_eventranges.pool.root'
+
+        if not hasattr(self, 'splitter_template'):
+            raise Exception('splitter_template is required but not defined.')
+        if not hasattr(self, 'default_input'):
+            raise Exception('default_input is required but not defined.')
+        if not hasattr(self, 'log_dir'):
+            raise Exception('log_dir is required but not defined')
+
+        self.request_queue = Queue.Queue()
+        self.output_queue = Queue.Queue()
+
+        self.processes = []
+
+    def get_splitter_process_name(self):
+        if hasattr(self, 'splitter_name'):
+            return self.splitter_name.replace('"', '').replace("'", "")
+        return "EventRange_Prefetcher"
+
+    def start_splitter_processes_old(self):
+        """
+        Start splitter process with multiple AthenaMP instances(one process per instance).
+        """
+        name = self.get_splitter_process_name()
+
+        with open(self.splitter_template, 'r') as f:
+            template = f.read()
+
+        for i in range(self.num_threads):
+            process_name = name + '_%s' % i
+            process_cmd = template.format(process_name=process_name, num_processes=1, output_prefix=self.output_prefix,
+                                          work_dir=self.log_dir, default_input=self.default_input)
+            today_str = datetime.date.today().strftime("%Y-%m-%d")
+            stdout = self.log_dir + '/' + process_name + '.stdout.' + today_str
+            stderr = self.log_dir + '/' + process_name + '.stderr.' + today_str
+            stdout = open(stdout, 'a+')
+            stderr = open(stderr, 'a+')
+            process = AtlasPrefetcher(self.request_queue, self.output_queue, process_name, process_cmd, stdout, stderr, self.num_threads)
+            self.processes.append(process)
+
+    def start_splitter_processes(self):
+        """
+        Start splitter process with one AthenaMP instance(multiple processes per instance).
+        """
+        name = self.get_splitter_process_name()
+
+        with open(self.splitter_template, 'r') as f:
+            template = f.read()
+
+        process_name = name
+        process_cmd = template.format(process_name=process_name, num_processes=self.num_threads,
+                                      output_prefix=self.output_prefix, work_dir=self.log_dir,
+                                      default_input=self.default_input)
+
+        # self.logger.info("Starting ATLAS prefetcher %s with command %s" % (process_name, process_cmd))
+
+        today_str = datetime.date.today().strftime("%Y-%m-%d")
+        stdout = self.log_dir + '/' + process_name + '.stdout.' + today_str
+        stderr = self.log_dir + '/' + process_name + '.stderr.' + today_str
+        stdout = open(stdout, 'a+')
+        stderr = open(stderr, 'a+')
+        process = AtlasPrefetcher(self.request_queue, self.output_queue, process_name, process_cmd, stdout, stderr, self.num_threads)
+        process.start()
+        self.processes.append(process)
+
+    def send_requests(self, reqs):
+        """
+        Send splitting requests
+        """
+        self.logger.info("Sending %s requests to Atlas Prefetcher" % len(reqs))
+        for req in reqs:
+            event_range = {'eventRangeID': '%s-%s-%s-%s' % (req.coll_id, req.content_id, req.min_id, req.max_id),
+                           # 'scope': req.scope,
+                           'LFN': req.name,
+                           'startEvent': req.min_id,
+                           'lastEvent': req.max_id,
+                           'PFN': req.pfn}
+            self.request_queue.put(event_range)
+
+    def get_queued_requests_num(self):
+        self.request_queue.qsize()
+
+    def need_more_requests(self):
+        if self.request_queue.qsize() < self.num_threads:
+            return True
+        else:
+            return False
+
+    def has_outputs(self):
+        return self.output_queue.qsize() > 0
+
+    def get_outputs(self):
+        """
+        Get splitted outputs
+        """
+        ret = []
+        for i in range(self.output_queue.qsize()):
+            output = self.output_queue.get()
+            output_ret = {'content_id': output[1],
+                          'coll_id': output[2],
+                          'size': output[2],
+                          'pfn': output[3]}
+            ret.append(output_ret)
+        return ret
+
+    def stop(self):
+        self.graceful_stop.set()
+
+    def run(self):
+
+        self.start_splitter_processes()
+        while not self.graceful_stop.is_set():
+            time.sleep(1)
+
+        for process in self.processes:
+            process.stop()
---
+++ lib/ess/daemons/splitter/daemon.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0OA
+#
+# Authors:
+# - Wen Guan, <wen.guan@cern.ch>, 2019
+
+
+import signal
+import time
+import traceback
+import Queue
+
+from ess.common.constants import Sections
+from ess.common.exceptions import ESSException, NoPluginException, DaemonPluginError
+from ess.common.utils import setup_logging
+from ess.core.catalog import add_contents, get_contents_by_edge, update_contents_by_id
+from ess.core.requests import get_requests, update_request
+from ess.daemons.common.basedaemon import BaseDaemon
+from ess.orm.constants import ContentType, ContentStatus, RequestStatus, GranularityType
+
+setup_logging(__name__)
+
+
+class Splitter(BaseDaemon):
+    """
+    The Splitter daemon class
+    """
+
+    def __init__(self, num_threads=1, **kwargs):
+        super(Splitter, self).__init__(num_threads, **kwargs)
+
+        self.config_section = Sections.Splitter
+        self.output_queue = Queue.Queue()
+
+        self.setup_logger()
+
+    def start_splitter_process(self):
+        if 'splitter' in self.plugins:
+            try:
+                self.logger.info("Starting splitter plugin %s" % self.plugins['splitter'])
+                self.plugins['splitter'].start()
+                self.logger.info("Splitter plugin %s started" % self.plugins['splitter'])
+            except Exception as error:
+                self.logger.error("Splitter plugin throws an exception: %s, %s" % (error, traceback.format_exc()))
+                raise DaemonPluginError("Splitter plugin throws an exception: %s" % (error))
+        else:
+            self.logger.critical("No available splitter plugins")
+            raise NoPluginException("No available splitter plugins")
+
+    def prepare_split_request_task(self):
+        """
+        Prepare split request
+        """
+        requests = get_requests(status=RequestStatus.PRECACHED, edge_name=self.resource_name)
+
+        if requests:
+            self.logger.info("Main thread get %s split requests" % len(requests))
+
+        for req in requests:
+            self.logger.info("Prepare to_split files for request %s" % req.request_id)
+            update_request(request_id=req.request_id, parameters={'status': RequestStatus.TOSPLITTING})
+            self.prepare_to_split_files(req)
+            update_request(request_id=req.request_id, parameters={'status': RequestStatus.SPLITTING})
+
+    def prepare_to_split_files(self, req):
+        if req.granularity_type == GranularityType.PARTIAL:
+            coll_id = req.processing_meta['coll_id']
+            files = get_contents_by_edge(edge_name=self.resource_name, coll_id=coll_id, content_type=ContentType.FILE)
+
+            sub_files = []
+            for file in files:
+                for i in range(file.min_id, file.max_id + 1, req.granularity_level):
+                    new_min_id = i
+                    new_max_id = i + req.granularity_level - 1
+                    if new_max_id > file.max_id:
+                        new_max_id = file.max_id
+                    new_file = {'coll_id': file.coll_id,
+                                'scope': file.scope,
+                                'name': file.name,
+                                'min_id': new_min_id,
+                                'max_id': new_max_id,
+                                'content_type': ContentType.PARTIAL,
+                                'edge_id': file.edge_id,
+                                'status': ContentStatus.TOSPLIT,
+                                'pfn': file.pfn,
+                                'priority': file.priority}
+
+                    sub_files.append(new_file)
+
+            self.logger.debug("Creating new splitting files: %s" % sub_files)
+            self.logger.info("Creating %s new splitting files" % len(sub_files))
+
+            add_contents(req.scope, req.name, self.resource_name, sub_files)
+
+    def get_splitter_tasks(self):
+        """
+        Get tasks to splitter
+        """
+        files = get_contents_by_edge(edge_name=self.resource_name, status=ContentStatus.TOSPLIT,
+                                     content_type=ContentType.PARTIAL)
+
+        # self.logger.debug("Main thread get %s files to split" % len(files))
+
+        update_files = {}
+        for file in files:
+            update_files[file.content_id] = {'status': ContentStatus.SPLITTING}
+        update_contents_by_id(update_files)
+
+        return files
+
+    def stage_out_outputs(self, outputs):
+        """
+        Stage out outputs
+        """
+        if 'stager' in self.plugins:
+            try:
+                self.logger.info("Starting stager plugin %s to stage out outputs" % self.plugins['stager'])
+                self.plugins['stager'].stage_out_outputs(self.output_queue, outputs)
+            except Exception as error:
+                self.logger.error("Splitter plugin throws an exception: %s, %s" % (error, traceback.format_exc()))
+                raise DaemonPluginError("Splitter plugin throws an exception: %s" % (error))
+        else:
+            for output in outputs:
+                self.output_queue.put(output)
+
+    def finish_splitter_tasks(self, files):
+        """
+        Finish processing the finished tasks, for example, update db status.
+        """
+        update_files = {}
+        for file in files:
+            update_files[file['content_id']] = {'status': ContentStatus.AVAILABLE,
+                                                'pfn_siz': file['size'],
+                                                'pfn': file['pfn']}
+        update_contents_by_id(update_files)
+
+    def run(self):
+        """
+        Main run function.
+        """
+        signal.signal(signal.SIGTERM, self.stop)
+
+        try:
+            self.logger.info("Starting main thread")
+
+            self.load_plugins()
+
+            self.start_splitter_process()
+
+            while not self.graceful_stop.is_set():
+                try:
+                    self.prepare_split_request_task()
+
+                    self.logger.debug("need_more_requests: %s" % self.plugins['splitter'].need_more_requests())
+                    if self.plugins['splitter'].need_more_requests():
+                        self.logger.info("Splitter plugin needs more events")
+
+                        files = self.get_splitter_tasks()
+                        if files:
+                            self.logger.info('Got %s files to split' % len(files))
+                            self.plugins['splitter'].send_requests(files)
+
+                    if self.plugins['splitter'].has_outputs():
+                        self.logger.info("Splitter plugin has outputs")
+
+                        outputs = self.plugins['splitter'].get_outputs()
+                        self.logger.info('Got %s splitted outputs' % len(outputs))
+
+                        self.stage_out_outputs(outputs)
+
+                    if self.output_queue.qsize() > 0:
+                        self.logger.info("Output stager has outputs")
+
+                        staged_outputs = []
+                        while not self.output_queue.empty():
+                            staged_outputs.append(self.output_queue.get())
+                        self.logger.info('Got %s staged outputs' % len(staged_outputs))
+
+                        self.finish_splitter_tasks(staged_outputs)
+
+                    time.sleep(5)
+                except ESSException as error:
+                    self.logger.error("Main thread ESSException: %s" % str(error))
+                except Exception as error:
+                    self.logger.critical("Main thread exception: %s\n%s" % (str(error), traceback.format_exc()))
+        except KeyboardInterrupt:
+            self.stop()
+
+
+if __name__ == '__main__':
+    daemon = Splitter()
+    daemon.run()
