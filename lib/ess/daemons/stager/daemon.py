469ae6052bae6eca2dd1239e8be8ba507a5aada6
Wen Guan <wen.guan@cern.ch>
2019-03-04 17:22:31 +0100

    add stager daemon

---
+++ lib/ess/daemons/stager/daemon.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0OA
+#
+# Authors:
+# - Wen Guan, <wen.guan@cern.ch>, 2019
+
+
+import signal
+import time
+import traceback
+import Queue
+
+from ess.common.constants import Sections
+from ess.common.exceptions import ESSException, NoPluginException, DaemonPluginError
+from ess.common.utils import setup_logging
+from ess.core.catalog import get_contents_by_edge, update_contents_by_id
+from ess.daemons.common.basedaemon import BaseDaemon
+from ess.orm.constants import ContentStatus
+
+setup_logging(__name__)
+
+
+class Stager(BaseDaemon):
+    """
+    The Stager daemon class
+    """
+
+    def __init__(self, num_threads=1, **kwargs):
+        super(Stager, self).__init__(num_threads, **kwargs)
+
+        self.config_section = Sections.Stager
+        self.request_queue = Queue.Queue()
+        self.finished_queue = Queue.Queue()
+
+        self.setup_logger()
+
+    def start_stagers(self):
+        if 'stager' in self.plugins:
+            try:
+                self.logger.info("Starting stager plugin %s" % self.plugins['stager'])
+                self.plugins['stager'].set_queues(self.request_queue, self.finished_queue)
+                self.plugins['stager'].start()
+                self.logger.info("Stager plugin %s started" % self.plugins['stager'])
+            except Exception as error:
+                self.logger.error("Stager plugin throws an exception: %s, %s" % (error, traceback.format_exc()))
+                raise DaemonPluginError("Stager plugin throws an exception: %s" % (error))
+        else:
+            self.logger.critical("No available stager plugins")
+            raise NoPluginException("No available stager plugins")
+
+    def stop_stagers(self):
+        if 'stager' in self.plugins:
+            try:
+                self.logger.info("Stopping stager plugin %s" % self.plugins['stager'])
+                self.plugins['stager'].stop()
+            except Exception as error:
+                self.logger.error("Stager plugin throws an exception: %s, %s" % (error, traceback.format_exc()))
+                raise DaemonPluginError("Stager plugin throws an exception: %s" % (error))
+
+    def is_stagers_alive(self):
+        if 'stager' in self.plugins:
+            try:
+                self.plugins['stager'].is_alive()
+            except Exception as error:
+                self.logger.error("Stager plugin throws an exception: %s, %s" % (error, traceback.format_exc()))
+                raise DaemonPluginError("Stager plugin throws an exception: %s" % (error))
+        return False
+
+    def get_stager_tasks(self):
+        """
+        Get tasks to stage out.
+        """
+        files = get_contents_by_edge(edge_name=self.resource_name, status=ContentStatus.SPLITED, content_type=None)
+
+        update_files = {}
+        for file in files:
+            update_files[file.content_id] = {'status': ContentStatus.STAGINGOUT}
+        update_contents_by_id(update_files)
+
+        for file in files:
+            to_stageout = {'content_id': file.content_id,
+                           'pfn_size': file.pfn_size,
+                           'pfn': file.pfn}
+            self.request_queue.put(to_stageout)
+
+    def finish_stager_tasks(self, files):
+        """
+        Finish processing the finished tasks, for example, update db status.
+        """
+
+        update_files = {}
+        while not self.request_queue.empty():
+            file = self.request_queue.get()
+            update_files[file['content_id']] = {'status': ContentStatus.AVAILABLE,
+                                                'pfn_size': file['pfn_size'],
+                                                'pfn': file['pfn']}
+        self.logger.info('Got %s staged outputs' % len(update_files))
+        update_contents_by_id(update_files)
+
+    def run(self):
+        """
+        Main run function.
+        """
+        signal.signal(signal.SIGTERM, self.stop)
+
+        try:
+            self.logger.info("Starting main thread")
+
+            self.load_plugins()
+
+            self.start_stagers()
+
+            while not self.graceful_stop.is_set():
+                try:
+                    if self.request_queue.qsize() < 1:
+                        self.get_stager_tasks()
+
+                    if self.finished_queue.qsize() > 0:
+                        self.logger.info("Output stager has outputs")
+                        self.finish_stager_tasks()
+
+                    time.sleep(5)
+                except ESSException as error:
+                    self.logger.error("Main thread ESSException: %s" % str(error))
+                except Exception as error:
+                    self.logger.critical("Main thread exception: %s\n%s" % (str(error), traceback.format_exc()))
+        except KeyboardInterrupt:
+            self.stop()
+        except Exception as error:
+            self.logger.error("Main thread ESSException: %s, %s" % (str(error), traceback.format_exc()))
+
+        self.stop_stagers()
+        while(self.is_stagers_alive()):
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    daemon = Stager()
+    daemon.run()
---
+++ lib/ess/daemons/stager/object_store_stager.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0OA
+#
+# Authors:
+# - Wen Guan, <wen.guan@cern.ch>, 2019
+
+
+"""
+Splitter plugin based ATLAS AthenaMP prefetcher
+"""
+
+import logging
+import os
+import threading
+import time
+import traceback
+
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+
+from ess.daemons.common.plugin_base import PluginBase
+
+
+class Stager(threading.Thread):
+
+    def __init__(self, request_queue, output_queue, logger=None, bucket_name=None,
+                 access_key=None, secret_key=None, hostname=None, port=None, is_secure=None,
+                 signed_url=True, lifetime=3600):
+
+        threading.Thread.__init__(self)
+        if logger:
+            self.logger = logger
+        else:
+            self.logger = logging.getLogger(self.__class__.__name__)
+        self.graceful_stop = threading.Event()
+
+        self.request_queue = request_queue
+        self.output_queue = output_queue
+
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.hostname = hostname
+        self.port = port
+        self.is_secure = is_secure
+        self.bucket_name = bucket_name
+
+        self.signed_url = signed_url
+        self.lifetime = lifetime
+
+        self.conn = self.connect_s3()
+        self.bucket = None
+
+    def stop(self):
+        self.graceful_stop.set()
+
+    def connect_s3(self):
+        return boto.connect_s3(aws_access_key_id=self.access_key,
+                               aws_secret_access_key=self.secret_key,
+                               host=self.hostname,
+                               port=self.port,
+                               is_secure=self.is_secure,
+                               calling_format=boto.s3.connection.OrdinaryCallingFormat())
+
+    def get_bucket_name(self, coll_id):
+        bucket_name = '%s-%s' % (self.bucket_name, coll_id)
+        return bucket_name
+
+    def get_bucket(self, coll_id):
+        self.conn = self.connect_s3()
+        try:
+            bucket = self.conn.get_bucket(self.get_bucket_name(coll_id))
+        except boto.exception.S3ResponseError as error:
+            self.logger.error("Failed to get bucket %s: %s" % (self.get_bucket_name(coll_id), error))
+            bucket = self.conn.create_bucket(self.get_bucket_name(coll_id))
+        return bucket
+
+    def stage_out(self, req):
+        try:
+            try:
+                if self.bucket is None:
+                    self.bucket = self.get_bucket(req['coll_id'])
+
+                key = Key(self.bucket, os.path.basename(req['pfn']))
+                key.set_contents_from_filename(req['pfn'])
+            except boto.exception.S3ResponseError as error:
+                self.bucket = self.get_bucket(req['coll_id'])
+                key = Key(self.bucket, os.path.basename(req['pfn']))
+                key.set_contents_from_filename(req['pfn'])
+
+            if key.size == req['size']:
+                self.logger.debug("Successfully staged out %s" % req['pfn'])
+                if self.signed_url:
+                    req['pfn'] = key.generate_url(self.lifetime, method='GET')
+                else:
+                    req['pfn'] = 's3://%s:%s/%s/%s' % (self.hostname,
+                                                       self.port,
+                                                       self.get_bucket_name(req['coll_id']),
+                                                       os.path.basename(req['pfn']))
+                return req
+            else:
+                self.logger.debug("Failed to stage out %s: size mismatch(local size: %s, remote size: %s)" % (
+                                  req['pfn'], req['size'], key.size))
+        except Exception as error:
+            self.logger.error("Failed to stageout request(%s): %s, %s" % (req, error, traceback.format_exc()))
+
+    def run(self):
+        while not self.graceful_stop.is_set():
+            try:
+                if not self.request_queue.empty():
+                    req = self.request_queue.get(False)
+                    if req:
+                        self.logger.debug("Staging out %s" % req)
+                        output = self.stage_out(req)
+                        if output:
+                            self.logger.debug("Successfully staged out: %s" % output)
+                            self.output_queue.put(output)
+                else:
+                    time.sleep(1)
+            except Exception as error:
+                self.logger.error("Stager throws an exception: %s, %s" % (error, traceback.format_exc()))
+
+
+class ObjectStoreStager(PluginBase):
+    def __init__(self, **kwargs):
+        super(ObjectStoreStager, self).__init__(**kwargs)
+        self.setup_logger()
+        self.graceful_stop = threading.Event()
+
+        if not hasattr(self, 'num_threads'):
+            self.num_threads = 1
+        else:
+            self.num_threads = int(self.num_threads)
+
+        if not hasattr(self, 'access_key'):
+            raise Exception('access_key is required but not defined.')
+        if not hasattr(self, 'secret_key'):
+            raise Exception('secret_key is required but not defined.')
+        if not hasattr(self, 'hostname'):
+            raise Exception('hostname is required but not defined.')
+        if not hasattr(self, 'port'):
+            raise Exception('port is required but not defined.')
+        else:
+            self.port = int(self.port)
+        if not hasattr(self, 'is_secure'):
+            raise Exception('is_secure is required but not defined.')
+        if not hasattr(self, 'bucket_name'):
+            raise Exception('bucket_name is required but not defined.')
+
+        if not hasattr(self, 'signed_url'):
+            self.signed_url = True
+        if not hasattr(self, 'lifetime'):
+            self.lifetime = 3600 * 24 * 7
+        else:
+            self.lifetime = int(self.lifetime)
+
+        self.request_queue = None
+        self.output_queue = None
+        self.threads = []
+
+    def set_queues(self, request_queue, output_queue):
+        self.request_queue = request_queue
+        self.output_queue = output_queue
+
+    def start(self):
+        for i in range(self.num_threads):
+            stager = Stager(self.request_queue,
+                            self.output_queue,
+                            logger=self.logger,
+                            bucket_name=self.bucket_name,
+                            access_key=self.access_key,
+                            secret_key=self.secret_key,
+                            hostname=self.hostname,
+                            port=self.port,
+                            is_secure=self.is_secure,
+                            signed_url=self.signed_url,
+                            lifetime=self.lifetime)
+            stager.start()
+            self.threads.append(stager)
+
+    def stop(self):
+        for thread in self.threads:
+            thread.stop()
+
+    def is_alive(self):
+        for thread in self.threads:
+            if thread.is_alive():
+                return True
+        return False
