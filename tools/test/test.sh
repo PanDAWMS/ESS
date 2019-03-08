72042194b11085bfe4e94299a197643f04f288fd
Wen Guan <wen.guan@cern.ch>
2019-03-08 16:08:40 +0100

    update test

--- tools/test/test.sh
+++ tools/test/test.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+CurrentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RootDir="$( dirname $( dirname "$CurrentDir" ))"
+echo $RootDir
+
+export ESS_HOME=$RootDir
+export PYTHONPATH=$RootDir/lib:$PYTHONPATH
+
 python -m unittest2 discover -v lib/ess/tests/ "test_*.py"
